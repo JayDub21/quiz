a7fecc536be7dec498fe27dd35b474f028162d45
JayDub21 <justin.wofford.21@gmail.com>
2019-12-17 16:55:43 -0600

    update

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      title: "Which of the following animals has horns?",
+      choices: ["Ibex", "Elk", "Moose", "all of the above"],
+      answer: "Ibex"
+    },
+    {
+      title: "Which of the following animals has antlers?",
+      choices: ["Elephant", "Red Stag", "Rhino", "Unicorn"],
+      answer: "Red Stag"
+    },
+    {
+        title: "Which of the following animals has 4 toes?",
+        choices: ["Wildebeast", "Mountain Goat", "Blackbuck", "Caribou"],
+        answer: "Caribou"
+      },
+      {
+        title: "What is the largest land animal in North America?",
+        choices: ["Moose", "Bison", "Elk", "Stuart Little"],
+        answer: "Bison"
+      },
+      {
+        title: "Which male duck has green feathers on its head?",
+        choices: ["Northern Pintail", "Canvasback", "Wood Duck", "American Wigeon"],
+        answer: "Bison"
+      }
+  ];
